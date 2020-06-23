3f7ac9693b54ea518a23dfe614a657a6cf26cdb7
Gocir <ridwanwanwan72@gmail.com>
2020-06-23 13:16:55 +0700

    Modified Variabel Type for Price

--- lib/models/trashReceiveModel.dart
+++ lib/models/trashReceiveModel.dart
@@ -4,17 +4,20 @@ class TrashReceiveModel{
   static const ID = "id";
   static const PRICE = "price";
   static const TRASH_NAME = "trashName";
+  static const IMAGE = "image";
   static const PARTNER_ID = "partnerId";
 
   String _id;
-  double _price;
+  int _price;
   String _trashName;
+  String _image;
   String _partnerId;
 
 //  getters
   String get id => _id;
-  double get price => _price;
+  int get price => _price;
   String get trashName => _trashName;
+  String get image => _image;
   String get partnerId => _partnerId;
 
 // public variable
@@ -23,6 +26,7 @@ class TrashReceiveModel{
     _id = snapshot.data[ID];
     _price = snapshot.data[PRICE];
     _trashName = snapshot.data[TRASH_NAME];
+    _image = snapshot.data[IMAGE];
     _partnerId = snapshot.data[PARTNER_ID];
   }
 
