109df7558e6bba49e947f6418d90e0dcd705bff6
Gocir <ridwanwanwan72@gmail.com>
2020-07-18 11:14:31 +0700

    Add function delete

--- lib/widgets/trashReceiveLisTile.dart
+++ lib/widgets/trashReceiveLisTile.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:lingkung_partner/providers/partnerProvider.dart';
+import 'package:lingkung_partner/models/partnerModel.dart';
 import 'package:lingkung_partner/providers/trashReceiveProvider.dart';
 import 'package:lingkung_partner/screens/updateTrashReceive.dart';
+import 'package:lingkung_partner/services/trashReceiveService.dart';
 import 'package:lingkung_partner/utilities/colorStyle.dart';
 import 'package:lingkung_partner/utilities/textStyle.dart';
 import 'package:provider/provider.dart';
 
-class TrashReceiveLisTile extends StatelessWidget {
+class TrashReceiveLisTile extends StatefulWidget {
+  final PartnerModel partnerModel;
+  TrashReceiveLisTile({this.partnerModel});
+  @override
+  _TrashReceiveLisTileState createState() => _TrashReceiveLisTileState();
+}
+
+class _TrashReceiveLisTileState extends State<TrashReceiveLisTile> {
+  TrashReceiveServices _trashReceiveService = TrashReceiveServices();
   @override
   Widget build(BuildContext context) {
-    final partner = Provider.of<PartnerProvider>(context);
+    // final partner = Provider.of<PartnerProvider>(context);
     final trashReceiveProvider = Provider.of<TrashReceiveProvider>(context);
-    
-    trashReceiveProvider.loadTrashReceiveByPartner(partner.businessPartner.uid);
+    trashReceiveProvider.loadTrashReceiveByPartner(widget.partnerModel.id);
 
     return ListView.builder(
         scrollDirection: Axis.vertical,
@@ -22,6 +30,7 @@ class TrashReceiveLisTile extends StatelessWidget {
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0)),
             child: ListTile(
+              isThreeLine: true,
               leading: Container(
                   height: MediaQuery.of(context).size.height,
                   width: 50,
@@ -44,21 +53,41 @@ class TrashReceiveLisTile extends StatelessWidget {
                 weight: FontWeight.w600,
               ),
               trailing: Wrap(
-                spacing: 10,
+                direction: Axis.vertical,
+                spacing: 8,
                 children: <Widget>[
-                  GestureDetector(
-                    child: Icon(Icons.edit, color: green),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => UpdateTrashReceivePage(trashReceiveModel: trashReceiveProvider.trashReceiveByPartner[index]),
-                          ));
-                    },
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: green,
+                    ),
+                    child: GestureDetector(
+                      child: Icon(Icons.edit, size: 14.0, color: white),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => UpdateTrashReceivePage(
+                                  trashReceiveModel: trashReceiveProvider
+                                      .trashReceiveByPartner[index]),
+                            ));
+                      },
+                    ),
                   ),
-                  GestureDetector(
-                    child: Icon(Icons.delete_outline, color: yellow),
-                    onTap: () {},
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: yellow,
+                    ),
+                    child: GestureDetector(
+                      child:
+                          Icon(Icons.delete_outline, size: 14.0, color: white),
+                      onTap: () async {
+                        await _trashReceiveService.deleteTrashReceive(trashReceiveProvider.trashReceiveByPartner[index].id);
+                      },
+                    ),
                   ),
                 ],
               ),
