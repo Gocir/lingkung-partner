e51e4ab14aa58e158226092c22ac0bee74368a93
Gocir <ridwanwanwan72@gmail.com>
2020-09-10 21:55:51 +0700

    Menambahkan style widget text

--- lib/utilities/textStyle.dart
+++ lib/utilities/textStyle.dart
@@ -3,24 +3,38 @@ import 'package:lingkung_partner/utilities/colorStyle.dart';
 
 class CustomText extends StatelessWidget {
   final String text;
-  final TextOverflow overflow;
-  final double size;
+  final TextAlign align;
+  final TextOverflow over;
+  final int line;
   final Color color;
+  final double size;
+  final FontStyle style;
   final FontWeight weight;
 
   //name constructor that has a positional parameters with the text required and the other parameters optional
-  CustomText({@required this.text, this.overflow, this.size, this.color, this.weight});
+  CustomText(
+      {@required this.text,
+      this.align,
+      this.over,
+      this.line,
+      this.color,
+      this.size,
+      this.style,
+      this.weight});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      overflow: overflow ?? null,
+      textAlign: align ?? TextAlign.left,
+      overflow: over,
+      maxLines: line,
       style: TextStyle(
+          color: color ?? black,
           fontFamily: "Poppins",
           fontSize: size ?? 14,
-          color: color ?? black,
-          fontWeight: weight ?? FontWeight.normal),
+          fontStyle: style,
+          fontWeight: weight),
     );
   }
 }
